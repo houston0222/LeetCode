4821bac14905fa3c39f81fbd2a593eacfff006b9
houston0222 <86028199+houston0222@users.noreply.github.com>
2025-06-13 17:26:53 +0100

    Time: 4 ms (34.81%), Space: 5 MB (18.43%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.go
+++ 0010-regular-expression-matching/0010-regular-expression-matching.go
@@ -1,75 +1,49 @@
 /*
-the difficult part is the *, because it will match zero or more of the preceding element, 
-so ".*" will match everything
-because of that we can't perform index by index comparsion.
+this use recursion + memo to solve, because * need backtrack to try multiple way
 
-I will loop the p, and p length is <= s length, so I will have compensatedSi to get correct campersion.
+dp(si, pi) means check if s[si:] match p[pi:]
 
-*/ 
+we store result in map so we don't repeat same check
+
+if p[pi+1] is '*', we can:
+1. skip the char and * (means 0 match)
+2. if current s[si] match p[pi], try match next s (keep p same)
+
+if no *, just check if current char match, and go next
+
+base case: if pattern finished, s must also finished
+*/
 
 func isMatch(s string, p string) bool {
+    memo := make(map[string]bool)
 
-    isMatched := true
+    var dp func(si, pi int) bool
 
-    si := 0
-    compensatedSi := 0
-    pi := 0
-    
-    
-    for pi < len(p) && si + compensatedSi < len(s) {
-        strsch := string(s[si + compensatedSi])
-        strpch := string(p[pi])
-
-        if strpch == "."{
-            isMatched = true
-            si ++ 
-            pi ++
-        } else if strpch == "*" {
-            previousStrpch := string(p[pi - 1])
-
-            sequentialMatched := true
-
-            for sequentialMatched == true && si + compensatedSi < len(s){
-                strsch := string(s[si + compensatedSi])
-
-                if previousStrpch == "."{
-                    compensatedSi ++  
-                } else {
-                    if (strsch == previousStrpch){
-                        compensatedSi ++  
-                    } else {
-                        sequentialMatched = false
-                    }
-                }
-            }
-
-            if sequentialMatched == true{
-                si ++ 
-                pi ++
-            } else {
-                isMatched = false
-                break
-            }
-
-        } else {
-            if strsch != strpch{
-                isMatched = false
-                break
-            } else {
-                si ++ 
-                pi ++
-            }
-
+    dp = func(si, pi int) bool {
+        key := fmt.Sprintf("%d,%d", si, pi)
+        if val, ok := memo[key]; ok {
+            return val
         }
 
+        if pi == len(p) {
+            return si == len(s)
+        }
+
+        match := false
+
+        currentMatch := si < len(s) && (s[si] == p[pi] || p[pi] == '.')
+
+        if pi+1 < len(p) && p[pi+1] == '*' {
+            match = dp(si, pi+2) || (currentMatch && dp(si+1, pi))
+        } else {
+            if currentMatch {
+                match = dp(si+1, pi+1)
+            }
+        }
+
+        memo[key] = match
+        return match
     }
 
-
-    if si+compensatedSi-1 != len(s){
-
-        return false
-    }
-
-    return isMatched
-    
+    return dp(0, 0)
 }
